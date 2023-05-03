fbd55f304f45b47fc7bdd18284c1ba554a0fb655
MiguelKoh <81209063+MiguelKoh@users.noreply.github.com>
2023-05-02 20:58:56 -0600

    cambios

--- 1_Estadistica.py
+++ 1_Estadistica.py
@@ -4,7 +4,6 @@ import streamlit.components.v1 as componets
 
 URL="https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
 dfIris= pd.read_csv(URL)
-
 st.title("Analisis estadistico Iris Dataset")
 
 componets.html("""<hr style="height:3px;border:none;color:#333"/hr>""")
